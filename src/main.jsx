e5afbd854c1353569db7d1f5ed38c26932af2df4
Waqar-ahmedkhan <waqarahmed44870@gmail.com>
2024-03-29 21:36:49 +0500

    using redux now

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
+import Provider from "@reduxjs/toolkit";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-
+import { store } from "./store/Store.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>
 );
---
+++ src/store/reducers/movieSlice.jsx
@@ -0,0 +1,3 @@
+const initalState = {
+  value: 0,
+};
